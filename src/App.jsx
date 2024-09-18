88407b36c14a6e8d44b5c1cc9a6b8d0a8cdca9f8
Abu Bakar Tahir <142303664+Abu-Bakar-T@users.noreply.github.com>
2024-09-18 07:18:12 +0000

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Abu Bakar Tahir",
+  title: "Unity Game Developer, Cloud Ethusiast, and MLSA-Alpha",
+  email: "ch.abu.bakar.tahir.zain@gmail.com",
+  gitHub: "https://github.com/Abu-Bakar-T",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/abu-bakar-tahir",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
